9e347aba508a1d7b95b5fb05de4796180266a0d4
Fakile Temitope Oluwaseyi <fakiletemitope@gmail.com>
2024-07-26 16:33:14 +0100

    initial

---
+++ script.js
@@ -0,0 +1,20 @@
+//string
+
+let first_name = "Shola"
+let last_name ="Oguns"
+let favorite_sport ="Basket ball"
+let password ="berauuirr"
+let color  = "Blue"
+
+const pie = 3.14
+let number1 = 5
+let age = 56
+const e = 2.7182818284
+let size = 2.5
+
+let is_admin = true
+let is_login = false
+
+let v = NaN
+
+let a = null
